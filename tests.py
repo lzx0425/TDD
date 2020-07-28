0b8242d2d67b1c5161cba8eab9d98bc5c81aeada
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 20:37:02 -0700

    make test case to test empty string

--- tests.py
+++ tests.py
@@ -5,34 +5,9 @@ class TestCase(unittest.TestCase):
 	def test1(self):
 		the_pwd = ""
 		self.assertFalse(check_pwd(the_pwd),msg="this is an empty password")
+		self.assertTrue(check_pwd(the_pwd),msg="this is an empty password")
+
 	
-	def test2_correct_pwd(self):
-		the_pwd = "Iloveyou3000!"
-		self.assertTrue(check_pwd(the_pwd),msg="this is the correct password")
-
-	def test_3_wrong_pwd_short_8(self):
-		the_pwd = "I<3u"
-		self.assertFalse(check_pwd(the_pwd),msg="length is too short")
-	
-	def test_4_wrong_pwd_greater_20(self):
-		the_pwd = "Iloveyoumorethan3000times!!<3"
-		self.assertFalse(check_pwd(the_pwd),msg="length is too long")
-
-	def test_5_wrong_pwd_no_lower(self):
-		the_pwd = "ILOVEYOU3000!"
-		self.assertFalse(check_pwd(the_pwd),msg="no lower case")
-
-	def test_6_wrong_pwd_no_upper(self):
-		the_pwd = "iloveyou3000!"
-		self.assertFalse(check_pwd(the_pwd),msg="no upper case")
-
-	def test_7_no_digit(self):
-		the_pwd = "Iloveyou!"
-		self.assertFalse(check_pwd(the_pwd),msg="no digit")
-
-	def test_8_no_special_case(self):
-		the_pwd = "Iloveyou3000"
-		self.assertFalse(check_pwd(the_pwd),msg="no special case")
 
 if __name__ == '__main__':
 	unittest.main()
