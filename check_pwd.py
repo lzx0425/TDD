286eb8e411d1b7c17ae7d3ab9412eb5f4d2af706
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 20:40:58 -0700

    make function to check special case

--- check_pwd.py
+++ check_pwd.py
@@ -1,3 +1,9 @@
+def char_check(pwd):
+	dig_check = False
+	special_char = {'~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','='}
+	if not any(char in special_char for char in pwd):
+		return False
+
 def check_pwd(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
