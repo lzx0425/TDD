c4d680e3c79c999a7364597d6f76d1f2dfd96172
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 19:56:57 -0700

    update check function

--- check_pwd.py
+++ check_pwd.py
@@ -1,39 +1,22 @@
-#def char_check(pwd):
-#	dig_check = False
-#	special_char = {'~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','='}
-#	if not any(char in special_char for char in pwd):
-#		return False
+def char_check(pwd):
+	dig_check = False
+	special_char = {'~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','='}
+	if not any(char in special_char for char in pwd):
+		return False
 
-#	if not any(char.islower() for char in pwd):
-#		return False
-#
-#	if not any(char.isupper() for char in pwd):
-#		return False
+	if not any(char.islower() for char in pwd):
+		return False
+
+	if not any(char.isupper() for char in pwd):
+		return False
 	
-#	for char in pwd:
-#		if char.isdigit():
-#			dig_check = True
+	for char in pwd:
+		if char.isdigit():
+			dig_check = True
 
-#	return True
+	return True
 
 def check_pwd(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
-	
-	special_char = ['~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','=']
-	digit_check = False
-
-	if not any(char in special_char for char in pwd_input):
-		return False
-	
-	if not any(char.islower() for char in pwd_input):
-		return False
-	
-	if not any(char.isupper() for char in pwd_input):
-		return False
-	for char in pwd_input:
-		if char.isdigit():
-			dig_check = True
-	
-	return True
-	#return char_check(pwd_input)
+	return char_check(pwd_input)
