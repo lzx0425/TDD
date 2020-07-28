f33082e9257967a44fcd0fca8d20dab444d6dd43
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 19:57:07 -0700

    Add files via upload

--- tests.py
+++ tests.py
@@ -2,27 +2,27 @@ import unittest
 from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
-	def empty_pwd(self):
+	def test1(self):
 		the_pwd = ""
 		self.assertFalse(check_pwd(the_pwd),msg="this is an empty password")
-
-	def correct_pwd(self):
+	
+	def test2(self):
 		the_pwd = "Iloveyou3000!"
 		self.assertTrue(check_pwd(the_pwd),msg="this is the correct password")
 
-	def wrong_pwd_short_8(self):
+	def test_3_wrong_pwd_short_8(self):
 		the_pwd = "I<3u"
-		slef.assertFalse(check_pwd(the_pwd),msg="length is too short")
+		self.assertFalse(check_pwd(the_pwd),msg="length is too short")
 	
-	def wrong_pwd_greater_20(self):
+	def test_4_wrong_pwd_greater_20(self):
 		the_pwd = "Iloveyoumorethan3000times!!<3"
 		self.assertFalse(check_pwd(the_pwd),msg="length is too long")
 
-	def wrong_pwd_no_lower(self):
+	def test_5_wrong_pwd_no_lower(self):
 		the_pwd = "ILOVEYOU3000!"
 		self.assertFalse(check_pwd(the_pwd),msg="no lower case")
 
-	def wrong_pwd_no_upper(self):
+	def test_6_wrong_pwd_no_upper(self):
 		the_pwd = "iloveyou3000!"
 		self.assertFalse(check_pwd(the_pwd),msg="no upper case")
 
