495a44d545c299b08d5f6901be058ae5a77b43c9
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 18:16:49 -0700

    Add files via upload

--- check_pwd.py
+++ check_pwd.py
@@ -5,7 +5,7 @@ def char_check(pwd):
 
 	return True
 
-def pwd_check(pwd_input):
+def check_pwd(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
 	return char_check(pwd_input)
