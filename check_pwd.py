ec841b7653a43a68ee868514cd477fdedf8c5f2c
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 18:13:58 -0700

    Add files via upload

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,9 @@ def char_check(pwd):
 	special_char = {'~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','='}
 	if not any(char in special_char for char in pwd):
 		return False
+
+	return True
+
 def pwd_check(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
