3e71f9f101747a5ce2b16a15beb56a7d3d21f876
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 19:17:01 -0700

    update check function

--- check_pwd.py
+++ check_pwd.py
@@ -19,12 +19,21 @@
 def check_pwd(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
+	
 	special_char = ['~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','=']
-	if not any(char in special_char for char in pwd):
+	digit_check = False
+
+	if not any(char in special_char for char in pwd_input):
 		return False
-	if not any(char.islower() for char in pwd):
+	
+	if not any(char.islower() for char in pwd_input):
 		return False
-	if not any(char.isupper() for char in pwd):
+	
+	if not any(char.isupper() for char in pwd_input):
 		return False
+	for char in pwd_input:
+		if char.isdigit():
+			dig_check = True
+	
 	return True
 	#return char_check(pwd_input)
