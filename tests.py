29b19d4e5bbc6cbbad6caf9c1495f4e4f6d73fd6
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 20:48:33 -0700

    correct length test pass

--- tests.py
+++ tests.py
@@ -13,7 +13,6 @@ class TestCase(unittest.TestCase):
 	def test_3_wrong_pwd_short_8(self):
 		the_pwd = "I<3u"
 		self.assertFalse(check_pwd(the_pwd),msg="length is too short")
-		self.assertTrue(check_pwd(the_pwd),msg="length is too short")
 
 
 if __name__ == '__main__':
