bd5804f23a80029313d9b7bad2e55d71fa88e460
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 20:36:41 -0700

    make a function to check the length

--- check_pwd.py
+++ check_pwd.py
@@ -1,24 +1,3 @@
-def char_check(pwd):
-	dig_check = False
-	special_char = {'~','`','!','@','#','$','%','^','&','*','(',')','_','+','-','='}
-	if not any(char in special_char for char in pwd):
-		return False
-
-	if not any(char.islower() for char in pwd):
-		return False
-
-	if not any(char.isupper() for char in pwd):
-		return False
-	
-	#if not any(char.isdigit() for char in pwd):
-	#	return False
-
-	for char in pwd:
-		if char.isdigit():
-			dig_check = True
-
-	return True and dig_check
-
 def check_pwd(pwd_input):
 	if len(pwd_input) > 20 or len(pwd_input) < 8:
 		return False
