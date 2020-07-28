fb7b942fb24ea2e7846a754097558429dda17592
lzx0425 <55958418+lzx0425@users.noreply.github.com>
2020-07-27 20:48:06 -0700

    check the length < 8

--- tests.py
+++ tests.py
@@ -10,6 +10,11 @@ class TestCase(unittest.TestCase):
 		the_pwd = "Iloveyou3000!"
 		self.assertTrue(check_pwd(the_pwd),msg="this is the correct password")
 
+	def test_3_wrong_pwd_short_8(self):
+		the_pwd = "I<3u"
+		self.assertFalse(check_pwd(the_pwd),msg="length is too short")
+		self.assertTrue(check_pwd(the_pwd),msg="length is too short")
+
 
 if __name__ == '__main__':
 	unittest.main()
